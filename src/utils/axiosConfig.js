43e4bdce2fa6cddf19e35eb8637b20546b32365e
MohammadJuneda <mohammadjunedaafreen@gmail.com>
2026-05-09 20:47:03 +0530

    fixed errors

--- src/utils/axiosConfig.js
+++ src/utils/axiosConfig.js
@@ -4,4 +4,15 @@ const instance = axios.create({
   baseURL: "https://homefood-api-uka6.onrender.com/api",
 });
 
+// ✅ ADD TOKEN AUTOMATICALLY
+instance.interceptors.request.use((config) => {
+  const token = localStorage.getItem("token");
+
+  if (token) {
+    config.headers.Authorization = `Bearer ${token}`;
+  }
+
+  return config;
+});
+
 export default instance;
